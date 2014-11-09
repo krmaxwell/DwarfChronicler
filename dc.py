eba0d13d4b7da8be5e86da04c478846497749524
Kyle Maxwell <kmaxwell@verisign.com>
2014-11-08 20:03:53 -0600

    Make it so

---
+++ dc.py
@@ -0,0 +1,20 @@
+import argparse
+import json
+import xmltodict
+
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("datafile", help="Name of Legends XML export")
+    args = argparser.parse_args()
+
+    with open(args.datafile, 'rb') as f:
+        ldata = f.read()
+
+    legends = xmltodict.parse(ldata, encoding='cp437')
+
+    json.dumps(legends, indent=2)
+
+
+if __name__ == "__main__":
+    main()
