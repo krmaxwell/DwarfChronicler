77a050c70eaed6bbfe1165275822ec2427770dc5
Kyle Maxwell <kmaxwell@verisign.com>
2014-11-14 19:06:53 -0600

    Read from directory

--- dc.py
+++ dc.py
@@ -1,14 +1,21 @@
 import argparse
 import json
+import os
 import xmltodict
 
 
 def main():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("datafile", help="Name of Legends XML export")
+    argparser.add_argument("directory", help="Directory with export data")
     args = argparser.parse_args()
 
-    with open(args.datafile, 'rb') as f:
+    dirlist = os.listdir(args.directory)
+    for each in dirlist:
+        if each.endswith('xml'):
+            datafile = each
+            break
+
+    with open(os.path.join(args.directory, datafile), 'rb') as f:
         ldata = f.read()
 
     legends = xmltodict.parse(ldata, encoding='cp437')
