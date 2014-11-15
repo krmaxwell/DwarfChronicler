470213958ea3d08f475e72a668422e08d20270c1
Kyle Maxwell <kmaxwell@verisign.com>
2014-11-14 20:15:32 -0600

    Parse other data files and read world name

--- dc.py
+++ dc.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import sys
 import xmltodict
 
 
@@ -13,11 +14,21 @@ def main():
     for each in dirlist:
         if each.endswith('xml'):
             datafile = each
-            break
+        elif each.endswith('world_history.txt'):
+            histfile = each
+        elif each.endswith('world_sites_and_pops.txt'):
+            sitefile = each
 
     with open(os.path.join(args.directory, datafile), 'rb') as f:
         ldata = f.read()
 
+    with open(os.path.join(args.directory, histfile), 'rb') as f:
+        hdata = f.readlines()
+
+    with open(os.path.join(args.directory, sitefile), 'rb') as f:
+        sdata = f.readlines()
+
+    sys.stderr.write('Parsing data for %s\n' % hdata[0])
     legends = xmltodict.parse(ldata, encoding='cp437')
 
     print json.dumps(legends, indent=2)
